e8dea0928d076b187c3b1b3cae4d1cd2dd642926
Henrik Vollesen <henrik@vollesen.dk>
2022-02-01 20:57:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+using System.Data;
+using System.Collections.Generic;
+
+namespace Covid19DataLogger2022
+{
+    class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+            // Pass cmd line string directly to ctor of Covid19_DataLogger - for now.
+            // In next iteration, parse it with a new ParseCommandline() that extracts JSON to a new ctor
+            string SettingsPath;
+
+            if (args.Length > 0)
+            {
+                string arg0 = args[0].ToLower().Trim();
+                if (arg0 == "-settingsfile")
+                {
+                    if (args.Length > 1)
+                    {
+                        SettingsPath = args[1];
+                        if (File.Exists(SettingsPath))
+                        {
+                            Covid19_DataLogger theLogger = new();
+                            theLogger.Log(File.ReadAllText(SettingsPath));
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
